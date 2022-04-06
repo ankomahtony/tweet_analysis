95908f512f4f235f2eed1c985d9e4d91b16411d6
Anthony Ankomah <tonyankomah1@gmail.com>
2022-04-06 09:50:52 +0000

    more features added

--- app.py
+++ app.py
@@ -1,56 +1,130 @@
 import streamlit as st
-from helper import make_stopwords, authenticate, get_user_tweeets, make_wordcloud
+import matplotlib.pyplot as plt 
+from helper import make_stopwords, authenticate,create_pie_chart, get_user_tweeets,count_values_in_column, make_wordcloud, preprocess, get_tweets
 
 st.title(" ☁️ Analyse Your Tweets ☁️ ") 
-st.subheader("Create a wordcloud out of your last 200 tweets!")
-st.text_input("Enter a twitter username to begin", key="name")
+seleected = st.selectbox('Pick one', ['An Account', 'A Tag'])
+if seleected == "An Account":
+    st.subheader("Create a wordcloud out of your last 200 tweets!")
+    st.text_input("Enter a twitter username to begin", key="name")
 
-if __name__ == "__main__":
-    
-    # if we didn't assign last_name yet, do it
-    if "last_name" not in st.session_state:
-        st.session_state.last_name = ""
-
-    # if the user entered a new name
-    if st.session_state.last_name != st.session_state.name:    
-    
-        # if we didn't load the stopwords yet, do it 
-        if "all_stopwords" not in st.session_state:
-            st.session_state.all_stopwords = make_stopwords()
-    
-        # if we didn't authenticate yet, do it 
-        if "api" not in st.session_state:    
-
-            st.session_state.api = authenticate(st.secrets["consumer_key"],
-                                            st.secrets["consumer_secret"],
-                                            st.secrets["access_token_key"],
-                                            st.secrets["access_token_secret"])
-                                            
-            st.session_state.api = authenticate(st.secrets["consumer_key"],
-                                            st.secrets["consumer_secret"],
-                                            st.secrets["access_token_key"],
-                                            st.secrets["access_token_secret"])
-
+    if __name__ == "__main__":
         
+        # if we didn't assign last_name yet, do it
+        if "last_name" not in st.session_state:
+            st.session_state.last_name = ""
+
+        # if the user entered a new name
+        if st.session_state.last_name != st.session_state.name:    
         
-        # try getting the tweets; if the username is incorrect, display the except message     
-        try:
-            outtweets = get_user_tweeets(st.session_state.name,st.session_state.api)
+            # if we didn't load the stopwords yet, do it 
+            if "all_stopwords" not in st.session_state:
+                st.session_state.all_stopwords = make_stopwords()
         
-            # check that the user has tweeted at least ten times, 
-            # if not display the except message 
-            try:
-                cat = outtweets[1]  
+            # if we didn't authenticate yet, do it 
+            if "api" not in st.session_state:    
+
+                st.session_state.api = authenticate(st.secrets["consumer_key"],
+                                                st.secrets["consumer_secret"],
+                                                st.secrets["access_token_key"],
+                                                st.secrets["access_token_secret"])
+                                                
+                st.session_state.api = authenticate(st.secrets["consumer_key"],
+                                                st.secrets["consumer_secret"],
+                                                st.secrets["access_token_key"],
+                                                st.secrets["access_token_secret"])
+
             
-                # display loading sign whie making the wordcould
-                st.spinner()
-                with st.spinner(text='We\'re building the wordcloud. Give it a sec...'):
-                    figure = make_wordcloud(st.session_state.all_stopwords, outtweets)
-                    st.pyplot(figure)
-                    st.balloons()
+            
+            # try getting the tweets; if the username is incorrect, display the except message     
+            try:
+                outtweets = get_user_tweeets(st.session_state.name,st.session_state.api)
+            
+                # check that the user has tweeted at least ten times, 
+                # if not display the except message 
+                try:
+                    cat = outtweets[1]  
+                
+                    # display loading sign whie making the wordcould
+                    st.spinner()
+                    with st.spinner(text='We\'re building the wordcloud. Give it a sec...'):
+                        figure = make_wordcloud(preprocess(outtweets))
+                        st.pyplot(figure)
+                        st.balloons()
+
+                        plt.savefig('twitter_new.png', format="png")
+
+                        with open("twitter_new.png", "rb") as file:
+                            btn = st.download_button(
+                                    label="Download image",
+                                    data=file,
+                                    file_name="wordcloud.png",
+                                    mime="image/png"
+                            )
+                    
+                except:
+                    st.markdown("This account has fewer than 2 tweets. Tweet more and come back later or try again.")  
                 
             except:
-                st.markdown("This account has fewer than 2 tweets. Tweet more and come back later or try again.")  
+                st.markdown("This account doesn't exist. Please try again.")  
+
+else:
+    st.subheader("Analyse your Twitter Tag")
+    st.text_input("Enter a twitter tag to begin", key="tag")  
+
+    if __name__ == "__main__":
+        
+        # if we didn't assign last_name yet, do it
+        if "last_tag" not in st.session_state:
+            st.session_state.last_tag = ""
+
+        # if the user entered a new name
+        if st.session_state.last_tag != st.session_state.tag:    
+        
+            # if we didn't authenticate yet, do it 
+            if "api" not in st.session_state:    
+
+                st.session_state.api = authenticate(st.secrets["consumer_key"],
+                                                st.secrets["consumer_secret"],
+                                                st.secrets["access_token_key"],
+                                                st.secrets["access_token_secret"])
+                                                
+                st.session_state.api = authenticate(st.secrets["consumer_key"],
+                                                st.secrets["consumer_secret"],
+                                                st.secrets["access_token_key"],
+                                                st.secrets["access_token_secret"])
+
             
-        except:
-            st.markdown("This account doesn't exist. Please try again.")        
+            
+            # try getting the tweets; if the username is incorrect, display the except message     
+            try:
+                tweets = get_tweets(st.session_state.tag,500, st.session_state.api)
+            
+                # check that the user has tweeted at least ten times, 
+                # if not display the except message 
+                try:
+                    tw_list = tweets  
+                
+                    # display loading sign whie making the wordcould
+                    st.spinner()
+
+                    with st.spinner(text='We\'re building the wordcloud. Give it a sec...'):
+                        tw_list_negative = tw_list[tw_list["sentiment"]=="negative"]
+                        tw_list_positive = tw_list[tw_list["sentiment"]=="positive"]
+                        tw_list_neutral = tw_list[tw_list["sentiment"]=="neutral"]
+                        pc = count_values_in_column(tw_list,"sentiment")
+                        # create data for Pie Chart
+                        names= ['{} {}%'.format(x,str(round(pc.loc[x,"Percentage"]))) for x in pc.index]
+                        data=pc["Percentage"]
+                        wordfig = make_wordcloud(str(tw_list["text"].values))
+                        st.pyplot(wordfig)
+                        piefig = create_pie_chart(data,names)
+                        st.pyplot(piefig)
+                        st.balloons()
+                        
+                    
+                except:
+                    st.markdown("This account has fewer than 2 tweets. Tweet more and come back later or try again.")  
+                
+            except:
+                st.markdown("This account doesn't exist. Please try again.")  
--- helper.py
+++ helper.py
@@ -1,9 +1,21 @@
 from telnetlib import AUTHENTICATION
 import matplotlib.pyplot as plt 
 import tweepy
+import sys
+import os
+import string
 import re
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
 import numpy as np
+import pandas as pd
+import nltk
+from textblob import TextBlob
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from langdetect import detect
+from nltk.stem import SnowballStemmer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.feature_extraction.text import CountVectorizer
+nltk.download('vader_lexicon')
 # from os import path
 # from PIL import Image
 
@@ -21,6 +33,51 @@ def get_user_tweeets(screen_name,api):
     outtweets = [tweet.text for tweet in alltweets] 
     return outtweets
 
+
+def percentage(part, whole):
+    return 100 * float(part)/float(whole)
+
+def get_tweets(keyword, no_of_tweet, api):
+    tweets = tweepy.Cursor(api.search_tweets, q=keyword).items(no_of_tweet)
+    return tweets
+
+def preprocess_tweets(tweets):
+    tweet_list = []
+    for tweet in tweets:
+        tweet_list.append(tweet.text)
+
+    tweet_list = pd.DataFrame(tweet_list)
+    tweet_list.drop_duplicates(inplace = True)
+    tw_list = pd.DataFrame(tweet_list)
+    remove_rt = lambda x: re.sub('RT @\w+: '," ",x)
+    rt = lambda x: re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",x)
+    tw_list["text"] = tw_list.text.map(remove_rt).map(rt)
+    tw_list["text"] = tw_list.text.str.lower()
+    #Calculating Negative, Positive, Neutral and Compound values
+
+    tw_list[['polarity', 'subjectivity']] = tw_list['text'].apply(lambda Text: pd.Series(TextBlob(Text).sentiment))
+    for index, row in tw_list['text'].iteritems():
+        score = SentimentIntensityAnalyzer().polarity_scores(row)
+        neg = score['neg']
+        neu = score['neu']
+        pos = score['pos']
+        comp = score['compound']
+        if neg > pos:
+            tw_list.loc[index, 'sentiment'] = "negative"
+        elif pos > neg:
+            tw_list.loc[index, 'sentiment'] = "positive"
+        else:
+            tw_list.loc[index, 'sentiment'] = "neutral"
+        tw_list.loc[index, 'neg'] = neg
+        tw_list.loc[index, 'neu'] = neu
+        tw_list.loc[index, 'pos'] = pos
+        tw_list.loc[index, 'compound'] = comp
+
+    return tw_list
+
+
+
+
 # making the wordcloud
 def make_stopwords():
     text_file = open("all_stopwords.txt", "r")
@@ -37,11 +94,87 @@ def preprocess(out):
 # twitter_mask = np.array(Image.open("twitter.png"))
 
 
-def make_wordcloud(st_words, out):
-    text = preprocess(out)
-    wordcloud = WordCloud(width=1800, height=900,stopwords=st_words,
-                        max_font_size=250, max_words=400,
-                        colormap='rainbow', collocations=True).generate(text)  
+# def make_wordcloud(st_words, out):
+#     text = preprocess(out)
+#     wordcloud = WordCloud(width=1800, height=900,stopwords=st_words,
+#                         max_font_size=250, max_words=400,
+#                         colormap='rainbow', collocations=True).generate(text)  
+
+#     # image_colors = ImageColorGenerator(twitter_mask)
+
+#     fig = plt.figure(figsize=(20,10), facecolor='k')
+#     # plt.imshow(wordcloud.recolor(color_func=image_colors), interpolation="bilinear")
+#     plt.imshow(wordcloud, interpolation="bilinear")
+#     plt.axis("off")
+#     return fig
+
+
+# my functions
+
+def percentage(part, whole):
+    return 100 * float(part)/float(whole)
+
+
+def preprocess_tweets(tweets):
+    tweet_list = []
+    for tweet in tweets:
+        tweet_list.append(tweet.text)
+
+    tweet_list = pd.DataFrame(tweet_list)
+    tweet_list.drop_duplicates(inplace = True)
+    tw_list = pd.DataFrame(tweet_list)
+    tw_list["text"] = tw_list[0]
+    
+    remove_rt = lambda x: re.sub('RT @\w+: '," ",x)
+    rt = lambda x: re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",x)
+    tw_list["text"] = tw_list.text.map(remove_rt).map(rt)
+    tw_list["text"] = tw_list.text.str.lower()
+    #Calculating Negative, Positive, Neutral and Compound values
+
+    tw_list[['polarity', 'subjectivity']] = tw_list['text'].apply(lambda Text: pd.Series(TextBlob(Text).sentiment))
+    for index, row in tw_list['text'].iteritems():
+        score = SentimentIntensityAnalyzer().polarity_scores(row)
+        neg = score['neg']
+        neu = score['neu']
+        pos = score['pos']
+        comp = score['compound']
+        if neg > pos:
+            tw_list.loc[index, 'sentiment'] = "negative"
+        elif pos > neg:
+            tw_list.loc[index, 'sentiment'] = "positive"
+        else:
+            tw_list.loc[index, 'sentiment'] = "neutral"
+        tw_list.loc[index, 'neg'] = neg
+        tw_list.loc[index, 'neu'] = neu
+        tw_list.loc[index, 'pos'] = pos
+        tw_list.loc[index, 'compound'] = comp
+
+    return tw_list
+
+def get_tweets(keyword, no_of_tweet, api):
+    tweets = tweepy.Cursor(api.search_tweets, q=keyword).items(no_of_tweet)
+    tw_list = preprocess_tweets(tweets)
+    return tw_list
+
+def count_values_in_column(data,feature):
+    total=data.loc[:,feature].value_counts(dropna=False)
+    percentage=round(data.loc[:,feature].value_counts(dropna=False,normalize=True)*100,2)
+    return pd.concat([total,percentage],axis=1,keys=['Total','Percentage'])
+
+def create_pie_chart(data, names):
+    # Create a circle for the center of the plot
+    my_circle=plt.Circle( (0,0), 0.7, color='white')
+    plt.pie(data, labels=names)
+    p=plt.gcf()
+    p.gca().add_artist(my_circle)
+    return plt
+    
+
+def make_wordcloud(out):
+    stopwords = set(STOPWORDS)
+    wordcloud = WordCloud(width=1000, height=500,stopwords=stopwords,
+                        max_font_size=250, max_words=100,repeat=True,
+                        colormap='rainbow', collocations=True).generate(out)  
 
     # image_colors = ImageColorGenerator(twitter_mask)
 
@@ -49,5 +182,4 @@ def make_wordcloud(st_words, out):
     # plt.imshow(wordcloud.recolor(color_func=image_colors), interpolation="bilinear")
     plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
-    # plt.savefig('twitter3.png', format="png")
     return fig
